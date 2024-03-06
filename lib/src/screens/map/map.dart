9a15ddb856bd48cd03628687e4d9c253f861fafe
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2024-03-06 12:04:47 +0200

    updated flutter

--- lib/src/screens/map/map.dart
+++ lib/src/screens/map/map.dart
@@ -14,26 +14,26 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-  return FlutterMap(
-    options: MapOptions(
-      initialCenter: LatLng(51.509364, -0.128928),
-      initialZoom: 9.2,
-    ),
-    children: [
-      TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'com.example.app',
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(51.509364, -0.128928),
+        initialZoom: 9.2,
       ),
-      RichAttributionWidget(
-        attributions: [
-          TextSourceAttribution(
-            'OpenStreetMap contributors',
-            onTap: () => print('OpenStreetMap attribution tapped!'),
-          ),
-        ],
-      ),
-      CurrentLocationLayer(),
-    ],
-  );
-}
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => print('OpenStreetMap attribution tapped!'),
+            ),
+          ],
+        ),
+        CurrentLocationLayer(),
+      ],
+    );
+  }
 }
